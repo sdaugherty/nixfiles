cc0df5c2311d3dfef3696f6db2625087d29551d2
Stephanie Daugherty <sdaugherty@gmail.com>
2026-03-17 21:45:12 -0400

    Added stephanie@mystra's ssh key globally

--- modules/common.nix
+++ modules/common.nix
@@ -19,7 +19,7 @@
     common_desktop.enable = lib.mkEnableOption "Common desktop services and applications";
   };
 
-  users.users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPmTkcqfVI29TNfSaLBs0sY37bF9GrXAuPN00C6zti/K stephanie@mystra'' ];
+  users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPmTkcqfVI29TNfSaLBs0sY37bF9GrXAuPN00C6zti/K stephanie@mystra'' ];
 
 
   config = lib.mkMerge [
