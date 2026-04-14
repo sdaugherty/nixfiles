f9552c724296563b0244d71a0c61a4775161f4be
Stephanie Daugherty <sdaugherty@gmail.com>
2026-04-14 02:32:44 -0400

    nixos-rebuild: mystra updated (Gen Generation 101, Build-date NixOS 2026-04-14 02:32:35)
    Current Build:
    
    Generation  Build-date           NixOS version            Kernel   Configuration Revision  Specialisation  Current
    101         2026-04-14 02:32:35  26.05.20260409.4c1018d   6.18.21  Unknown                 []              True

--- modules/amd_desktop.nix
+++ modules/amd_desktop.nix
@@ -9,9 +9,26 @@
   options.myConfig.modules.amd_desktop.enable = lib.mkEnableOption "AMD graphics drivers and configuration";
 
   config = lib.mkIf config.myConfig.modules.amd_desktop.enable {
+    boot.initrd.kernelModules = [ "amdgpu" ];
+    services.xserver.videoDrivers = [ "amdgpu" ];
+
     hardware.graphics = {
       enable = true;
       enable32Bit = true;
+      extraPackages = with pkgs; [
+        libva
+        libva-vdpau-driver
+        libvdpau-va-gl
+      ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        libva
+        libva-vdpau-driver
+        libvdpau-va-gl
+      ];
     };
+
+    environment.systemPackages = with pkgs; [
+      libva-utils
+    ];
   };
 }
