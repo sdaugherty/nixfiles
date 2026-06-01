597f61958ef3d7030b1abe2e77abdf20f73601f5
Stephanie Daugherty <sdaugherty@gmail.com>
2026-06-01 02:14:51 -0400

    nixos-rebuild: mystra updated (Gen Generation 119, Build-date NixOS 2026-05-29 14:03:07)
    Current Build:
    
    Generation  Build-date           NixOS version           Kernel  Configuration Revision  Specialisation  Current
    119         2026-05-29 14:03:07  26.05.20260523.64c08a7  6.18.8  Unknown                 []              True

--- modules/dev_desktop.nix
+++ modules/dev_desktop.nix
@@ -10,7 +10,7 @@
   options.myConfig.modules.dev_desktop.enable = lib.mkEnableOption "Desktop development tools";
 
   config = lib.mkIf config.myConfig.modules.dev_desktop.enable {
-    nixpkgs.overlays = [ inputs.claude-desktop.overlays.default ];
+    # nixpkgs.overlays = [ inputs.claude-desktop.overlays.default ];
     nixpkgs.config.android_sdk.accept_license = true; # required for android-studio
 
     environment.systemPackages = with pkgs; [
@@ -23,7 +23,7 @@
       jetbrains.datagrip
       android-studio
       vscode.fhs
-      claude-desktop
+      # claude-desktop
       nerd-fonts.hack
       nerd-fonts.jetbrains-mono
       nerd-fonts.fira-mono
